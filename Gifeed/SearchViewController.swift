e9bed65ad7e5e8856fc22beb80d10910428c56a6
Edwin Rodríguez <edwinrodriguez@Edwins-MacBook-Pro.local>
2015-06-21 16:02:42 -0400

    Insert the views needed with their components and view controllers.

--- Gifeed/SearchViewController.swift
+++ Gifeed/SearchViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchViewController.swift
 //  Gifeed
 //
 //  Created by Edwin Rodriguez on 6/21/15.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
